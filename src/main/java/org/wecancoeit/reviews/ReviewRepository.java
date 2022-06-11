3cbc3de11661a0ff08d5a6f34ea9afe09bd07499
Firestone <danfires@amazon.com>
2022-06-11 00:25:49 -0400

    first find test passed

--- src/main/java/org/wecancoeit/reviews/ReviewRepository.java
+++ src/main/java/org/wecancoeit/reviews/ReviewRepository.java
@@ -1,5 +1,6 @@
 package org.wecancoeit.reviews;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,10 +8,17 @@ public class ReviewRepository {
 
     Map<Long, Review> reviewsList = new HashMap<>();
 
-    public ReviewRepository(Review review1) {
+    public ReviewRepository(Review... reviewsToAdd) {
+        for(Review review: reviewsToAdd){
+            reviewsList.put(review.getId(), review);
+        }
     }
 
     public Review findOne(long id) {
         return reviewsList.get(id);
+    }
+
+    public Collection<Review> findAll() {
+        return reviewsList.values();
     }
 }
--- src/test/java/org/wecancoeit/reviews/ReviewRepositoryTest.java
+++ src/test/java/org/wecancoeit/reviews/ReviewRepositoryTest.java
@@ -7,16 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ReviewRepositoryTest {
 
     private ReviewRepository underTest;
-    private Review review1 = new Review(1L, "Movie Review1", "imgr.com/???1",
+    private Review reviewOne = new Review(1L, "Movie Review1", "imgr.com/???1",
             "Movies", "This movie is not so good");
-    private Review review2 = new Review(2L, "Food Review", "imgr.com/???2",
+    private Review reviewTwo = new Review(2L, "Food Review", "imgr.com/???2",
             "Food", "The food is okay");
 
     @Test
-    public void shouldFindCourseOne(){
-        underTest = new ReviewRepository(review1);
+    public void shouldFindReviewOne(){
+        underTest = new ReviewRepository(reviewOne);
         Review foundReview = underTest.findOne(1L);
-        assertEquals(review1, foundReview);
+        assertEquals(reviewOne, foundReview);
     }
 
 }
