f59df9409df75bc908cccdeaf9368cb80e487db5
Firestone <danfires@amazon.com>
2022-06-11 00:32:18 -0400

    Second findall test passed

--- src/test/java/org/wecancoeit/reviews/ReviewRepositoryTest.java
+++ src/test/java/org/wecancoeit/reviews/ReviewRepositoryTest.java
@@ -2,21 +2,31 @@ package org.wecancoeit.reviews;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReviewRepositoryTest {
 
     private ReviewRepository underTest;
-    private Review reviewOne = new Review(1L, "Movie Review1", "imgr.com/???1",
+    private Review review1 = new Review(1L, "Movie Review1", "imgr.com/???1",
             "Movies", "This movie is not so good");
-    private Review reviewTwo = new Review(2L, "Food Review", "imgr.com/???2",
+    private Review review2 = new Review(2L, "Food Review", "imgr.com/???2",
             "Food", "The food is okay");
 
     @Test
-    public void shouldFindReviewOne(){
-        underTest = new ReviewRepository(reviewOne);
+    public void shouldFindReview1(){
+        underTest = new ReviewRepository(review1);
         Review foundReview = underTest.findOne(1L);
-        assertEquals(reviewOne, foundReview);
+        assertEquals(review1, foundReview);
+    }
+
+    @Test
+    public void shouldFindCourse1And2(){
+        underTest = new ReviewRepository(review1, review2);
+        Collection<Review> foundCourses = underTest.findAll();
+        assertThat(foundCourses).contains(review1, review2);
     }
 
 }
